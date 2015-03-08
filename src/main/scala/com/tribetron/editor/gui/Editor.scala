0bef5eaad48e6fb2378f4e8de4fac4e3490798b2
Humakt83 <urlact@yahoo.com>
2015-03-08 23:02:18 +0200

    Inputfields for rows and columns

--- src/main/scala/com/tribetron/editor/gui/Editor.scala
+++ src/main/scala/com/tribetron/editor/gui/Editor.scala
@@ -5,7 +5,7 @@ import scalafx.application.JFXApp.PrimaryStage
 import scalafx.geometry.Insets
 import scalafx.scene._
 import scalafx.scene.effect.DropShadow
-import scalafx.scene.layout.HBox
+import scalafx.scene.layout.{ HBox, VBox }
 import scalafx.scene.paint.Color._
 import scalafx.scene.paint.{ Stops, LinearGradient }
 import scalafx.scene.text.Text
@@ -14,6 +14,7 @@ import scalafx.scene.image.{ ImageView, Image }
 import scalafx.collections.ObservableBuffer
 import scalafx.scene.layout.{ GridPane }
 import scalafx.scene.Node
+import scalafx.scene.control.{ TextField, Label }
 import com.tribetron.editor.objects.GameObject
 
 object Editor extends JFXApp {
@@ -21,23 +22,36 @@ object Editor extends JFXApp {
   stage = new PrimaryStage {
     title = "Tribetron Editor"
     scene = new Scene {
-      val images = createImagePanel
-      content = images
+      val mainGroup = new Group {}
+      val vBox = new VBox()
+      vBox.children.add(createImagePanel)
+      vBox.children.add(createInputFields)
+      mainGroup.children.add(vBox)
+      content = mainGroup
     }
   }
 
   private def createImagePanel: Node = {
-    val imagePanel = new Group {}
     val box = new HBox()
     GameObjects.gameObjects.foreach(gameObject => box.children.add(createGameImage(gameObject.imageUrl)))
-    imagePanel.children.add(box)
-    imagePanel
+    box
+  }
+
+  private def createInputFields: Node = {
+    val box = new HBox()
+    val xField = new TextField() { this.maxWidth = 40.0 }
+    val yField = new TextField() { this.maxWidth = 40.0 }
+    box.children.add(new Label("Columns:"))
+    box.children.add(xField)
+    box.children.add(new Label("Rows:"))
+    box.children.add(yField)
+    box
   }
 
   private def createGameImage(imageUrl: String): Node = {
     new ImageView(new Image(this, imageUrl)) {
-      this.fitHeight = 50.0
-      this.fitWidth = 50.0
+      this.fitHeight = 30.0
+      this.fitWidth = 30.0
     }
   }
 }
