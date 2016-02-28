d48638b430c4984c37c2f8896e36be007d0b5e7c
Humakt83 <urlact@yahoo.com>
2016-02-29 00:03:39 +0200

    Update sbt and readme

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Adventure map editor for tribetron
 
 Creates json files using json4s and ScalaFX as GUI.
+
+###Eclipse
+Run sbt on console on project folder, then run commands compile and eclipse
--- src/main/scala/com/tribetron/editor/gui/BattleDialog.scala
+++ src/main/scala/com/tribetron/editor/gui/BattleDialog.scala
@@ -1,9 +1,14 @@
 package com.tribetron.editor.gui
 
 import com.tribetron.editor.objects.Opponent
+import scalafx.scene.control.{ TextField, Label, Button, Dialog, ButtonType }
 
-class BattleDialog {
+class BattleDialog extends Dialog {
   
+  title = "Battlemap options"
+  headerText = "Set options for battlemap"
+  
+    
 }
 
-case class Result(val maxRoster: Int, val reward: Int, val opponentTeamName: String, val opponent: Opponent.Opponent, val rounds: Int)
+case class BattleMapSettings(val maxRoster: Int, val reward: Int, val opponentTeamName: String, val opponent: Opponent.Opponent, val rounds: Int)
